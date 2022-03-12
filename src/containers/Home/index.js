a665832c41ac91f252ad8541b1b36bcbc5579b0e
Carlos yaco <cyaco33@gmail.com>
2022-03-12 11:16:59 -0500

    setup PokemonReducer and dispath

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { setPokemon } from "../../actions";
+import { getPokemons } from "../../api/getPokemons";
 import { PokemonList } from "../../components/PokemonList";
 import Searcher from "../../components/Searcher";
 import "./styles.css";
 
-function Home() {
+const mapStateToProps = (state) => ({
+  list: state.list,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemon(value)),
+});
+
+function Home({ list, setPokemons }) {
+  useEffect(() => {
+    getPokemons().then((res) => setPokemons(res.results));
+  }, []);
   return (
     <div className="Home">
       <Searcher />
@@ -12,4 +26,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
